d7bc309053e0ed0693bfbd8050bd4b56b7eafb8e
Brian Crawford <brian.crawford@spglobal.com>
2018-05-01 16:13:16 -0600

    Create ProductListComponent

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'pm-root',
   template: `
-    <div><h1>{{pageTitle}}</h1>
-      <div>My First Component</div>
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
     </div>
     `
 })
